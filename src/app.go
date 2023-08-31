9c0ab6476cd55de863d9f76e5359dc10ddf8e9ea
Indulekha Singh <indulekha.singh@merce.co>
2023-08-31 20:03:03 +0530

    #16514 - IsValidCountryCode2 function

--- src/app.go
+++ src/app.go
@@ -8,6 +8,12 @@ import (
 
 func main() {
 	fmt.Println("in main")
-	fmt.Println(valid.IsValidIndiaZip("111100"))
+	fmt.Println(valid.CleanerTest("clean file"))
+	fmt.Println(valid.ValidatorTest("validator file"))
+	fmt.Println(valid.IsValidIndiaZip("457845"))
+
+	fmt.Println("Valid CC2 in: ", valid.IsValidCountryCode2("in"))
+	fmt.Println("Valid CC2 IN: ", valid.IsValidCountryCode2("IN"))
+	fmt.Println("Invalid CC2 II: ", valid.IsValidCountryCode2("II"))
 
 }
--- util/customValidator.go
+++ util/customValidator.go
@@ -2,7 +2,12 @@ package util
 
 import (
 	"regexp"
+	"strings"
+
+	"github.com/biter777/countries"
 )
+
+const DEFAULT_COUNTRY = countries.India
 
 func ValidatorTest(str string) string {
 	return "Hello " + str
@@ -10,4 +15,8 @@ func ValidatorTest(str string) string {
 
 func IsValidIndiaZip(val string) bool {
 	return regexp.MustCompile(`^[1-9][0-9]{5}$`).MatchString(val)
+}
+
+func IsValidCountryCode2(val string) bool {
+	return (DEFAULT_COUNTRY.Alpha2() == strings.ToUpper(val))
 }
