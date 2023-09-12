4557b00f9d3f0035ca5e0efbee6cd99de6f1790f
unknown <adnya.sonawane@merce.co>
2023-09-12 13:51:37 +0530

    refs

--- src/app_code.go
+++ src/app_code.go
@@ -39,20 +39,20 @@ func main() {
 /* output
 
 PS D:\Merce\GoLang\go-framework\src> go run .\app_code.go
-Please enter the numer :-12345.400
+Please enter the number :-12345.400
 Entered Number:  -12345.400390625
 In words(India): minus twelve thousand three hundred forty-five Rupees and forty Paise
 Entered Number:  -12345.400390625
 In words(International): minus twelve thousand three hundred forty-five and forty
 
-Please enter the numer :12334434534534545.7000
+Please enter the number :12334434534534545.7000
 Entered Number:  1.2334434183282688e+16
 In words(India): twelve padma thirty-three neel forty-four kharab thirty-four arab fifty-three crore forty-five lakh thirty-four thousand five hundred forty-five Rupees and seventy Paise
 Entered Number:  1.2334434183282688e+16
 In words(International): twelve quadrillion three hundred thirty-four trillion four hundred thirty-four billion five hundred thirty-four million five hundred thirty-four thousand five hundred forty-five and seventy
 
 PS D:\Merce\GoLang\go-framework\src> go run .\app_code.go
-Please enter the numer :12345678901234567.43445
+Please enter the number :12345678901234567.43445
 Entered Number:  1.2345678407663616e+16
 In words(India): twelve padma thirty-four neel fifty-six kharab seventy-eight arab ninety crore twelve lakh thirty-four thousand five hundred sixty-seven Rupees and forty-three Paise
 Entered Number:  1.2345678407663616e+16
