c425a777c4e193ac13f84e8a703fefe6937f932e
Sachin Divekar <sachin.divekar@remiges.tech>
2023-10-26 23:49:14 +0530

    Remove non-rigel tables and queries

--- internal/pg/migrations/001_erptest.sql
+++ internal/pg/migrations/001_erptest.sql
@@ -1,17 +1,3 @@
-CREATE TABLE Employees (
-                           employee_id SERIAL PRIMARY KEY, -- Unique identifier for each employee
-                           name VARCHAR(100), -- Employee name
-                           title VARCHAR(50), -- Job title
-                           department VARCHAR(50) -- Department name
-);
-CREATE TABLE Vouchers (
-                          voucher_id SERIAL PRIMARY KEY, -- Unique ID for each voucher
-                          employee_id INT NOT NULL REFERENCES Employees(employee_id), -- Foreign Key reference to Employees table,
-                          date_of_claim DATE NOT NULL, -- The claim's date; no claim without a date
-                          amount DOUBLE PRECISION NOT NULL CHECK (amount > 0), -- Claimed amount; must be positive
-                          description TEXT -- Description of the claim
-);
-
 CREATE TABLE schema (
                         id SERIAL PRIMARY KEY,
                         name VARCHAR(100) UNIQUE NOT NULL,
@@ -43,8 +29,6 @@ CREATE TABLE config (
 
 ---- create above / drop below ----
 
-DROP TABLE IF EXISTS Vouchers;
-DROP TABLE IF EXISTS Employees;
 DROP TABLE IF EXISTS config_versions;
 DROP TABLE IF EXISTS config;
 DROP TABLE IF EXISTS schema_versions;
